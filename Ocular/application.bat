4ad7da657ef32ac2f269ceb3c03d901ed95da543
Aditya Arora <adi.arora.797@gmail.com>
2019-01-21 21:50:25 +0530

    Add files via upload

---
+++ Ocular/application.bat
@@ -0,0 +1,9 @@
+@echo off
+python application.py
+
+echo "Thank you for using Ocular."
+echo.
+echo "Kindly open readme.txt for future refrence. Or run application.bat for using the Ocular module."
+
+pause
+exit
